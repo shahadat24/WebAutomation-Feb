0e8a6ee0e1f24125e58911744996e6edaf1a4c1b
Md Hossain <mdhossain@Mds-MacBook-Air-3.local>
2019-05-18 16:59:01 -0400

    first commit

--- Mountsinai/src/main/java/mountsinaiPackage/MountsinaiHomePage.java
+++ Mountsinai/src/main/java/mountsinaiPackage/MountsinaiHomePage.java
@@ -1,6 +1,7 @@
 package mountsinaiPackage;
 
 import base.CommonAPI;
+import org.openqa.selenium.By;
 
 public class MountsinaiHomePage extends CommonAPI {
 
@@ -8,6 +9,8 @@ public class MountsinaiHomePage extends CommonAPI {
     public void HomePage(){
 
         System.out.println(driver.getTitle());
+
+        //driver.findElement(By.xpath("/html/body/header/div[3]/div/nav[2]/div/ul[2]/li[2]/a")).click();
 
 
 
